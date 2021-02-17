64011cf236e63e6d40e83691f9f20f80d2c160b7
Keertiraj Laxman Malik <KeertirajLaxmanMalik@fico.com>
2021-02-17 18:16:03 +0530

    created the heading and the white container

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,30 +1,71 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:pocket_friend/widgets/expense_list.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xff27c791),
-      body: Container(
-        padding:
-            EdgeInsets.only(top: 60.0, left: 30.0, right: 30.0, bottom: 30.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Row(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            padding: const EdgeInsets.only(
+                top: 60.0, bottom: 30.0, left: 30.0, right: 30.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Icon(
-                  Icons.menu,
-                  size: 40.0,
+                Row(
+                  children: [
+                    Icon(
+                      Icons.drag_handle,
+                      size: 40.0,
+                    ),
+                    Spacer(),
+                    CircleAvatar(
+                      backgroundImage:
+                          AssetImage('assets/images/profile_image.jpg'),
+                    ),
+                  ],
                 ),
-                Spacer(),
-                CircleAvatar(
-                  backgroundImage: AssetImage('images/profile_image.jpg'),
+                SizedBox(height: 20.0),
+                Text(
+                  'My budget',
+                  style: GoogleFonts.nunitoSans(
+                    textStyle: TextStyle(
+                      color: Colors.white,
+                      fontSize: 30.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
                 ),
+                SizedBox(height: 10.0),
+                Text(
+                  '₹ 5432.00',
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 30.0,
+                      fontWeight: FontWeight.bold),
+                ),
+                SizedBox(height: 20.0),
               ],
             ),
-          ],
-        ),
+          ),
+          Expanded(
+            child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 20.0),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(30.0),
+                  topRight: Radius.circular(30.0),
+                ),
+              ),
+              child: ExpenseList(),
+            ),
+          ),
+        ],
       ),
     );
   }
