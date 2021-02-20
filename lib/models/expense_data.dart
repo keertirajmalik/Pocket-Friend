a3816f922d694a349df6214432cbb587fd8ba962
Keertiraj Laxman Malik <KeertirajLaxmanMalik@fico.com>
2021-02-20 19:02:56 +0530

    added different expenses

--- lib/models/expense_data.dart
+++ lib/models/expense_data.dart
@@ -3,5 +3,7 @@ import 'package:pocket_friend/models/expense.dart';
 class ExpenseData {
   List<Expense> expenseAmount = [
     Expense(expense: 'Electricity', amount: '270'),
+    Expense(expense: 'Food', amount: '470'),
+    Expense(expense: 'Home', amount: '570'),
   ];
 }
