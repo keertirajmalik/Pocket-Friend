e01f6bae18a526b967114c626b25b378a2fcd9ff
Keertiraj Laxman Malik <KeertirajLaxmanMalik@fico.com>
2021-02-20 19:19:18 +0530

    added the method to calculate the total expense

--- lib/models/expense_data.dart
+++ lib/models/expense_data.dart
@@ -6,4 +6,12 @@ class ExpenseData {
     Expense(expense: 'Food', amount: '470'),
     Expense(expense: 'Home', amount: '570'),
   ];
+
+  String get totalExpense {
+    int totalExpense = 0;
+    for (var expense in expenseAmount) {
+      totalExpense += int.parse(expense.amount);
+    }
+    return totalExpense.toString();
+  }
 }
--- lib/widgets/expense_total_list.dart
+++ lib/widgets/expense_total_list.dart
@@ -1,30 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:pocket_friend/models/expense_data.dart';
 
 class ExpenseTotalList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Column(
+    return Row(
       children: [
-        Row(
-          children: [
-            Text(
-              '17 Feb 2021',
-              style: TextStyle(
-                color: Colors.black,
-                fontWeight: FontWeight.bold,
-                fontSize: 20.0,
-              ),
-            ),
-            Spacer(),
-            Text(
-              '270',
-              style: TextStyle(
-                fontSize: 20.0,
-                color: Colors.orange.shade400,
-                fontWeight: FontWeight.w900,
-              ),
-            ),
-          ],
+        SizedBox(height: 10.0),
+        Text(
+          '17 Feb 2021',
+          style: TextStyle(
+            color: Colors.black,
+            fontWeight: FontWeight.bold,
+            fontSize: 20.0,
+          ),
+        ),
+        Spacer(),
+        Text(
+          ExpenseData().totalExpense,
+          style: TextStyle(
+            fontSize: 20.0,
+            color: Colors.orange.shade400,
+            fontWeight: FontWeight.w900,
+          ),
         ),
       ],
     );
