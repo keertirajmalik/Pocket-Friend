e2b23707f642011a8b533de7cfe72a0e1c5d2ee8
Keertiraj Laxman Malik <KeertirajLaxmanMalik@fico.com>
2021-02-23 21:57:34 +0530

    added padding between screen and the Icon list

--- lib/widgets/icons.dart
+++ lib/widgets/icons.dart
@@ -3,35 +3,36 @@ import 'package:flutter/material.dart';
 class AppIcons extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(18.0),
-      decoration: BoxDecoration(
-        color: Color(0xff001827),
-        borderRadius: BorderRadius.all(
-          Radius.circular(50.0),
+    return Padding(
+      padding: const EdgeInsets.all(1.0),
+      child: Container(
+        padding: const EdgeInsets.all(15.0),
+        decoration: BoxDecoration(
+          color: Color(0xff001827),
+          borderRadius: BorderRadius.all(
+            Radius.circular(50.0),
+          ),
         ),
-      ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: [
-          Icon(
-            Icons.account_balance_wallet_rounded,
-            size: 40.0,
-            color: Colors.white,
-          ),
-          SizedBox(width: 20.0),
-          Icon(
-            Icons.notifications_outlined,
-            size: 40.0,
-            color: Colors.white,
-          ),
-          SizedBox(width: 20.0),
-          Icon(
-            Icons.add,
-            size: 40.0,
-            color: Colors.white,
-          ),
-        ],
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Icon(
+              Icons.account_balance_wallet_rounded,
+              size: 40.0,
+              color: Colors.white,
+            ),
+            Icon(
+              Icons.notifications_outlined,
+              size: 40.0,
+              color: Colors.white,
+            ),
+            Icon(
+              Icons.add,
+              size: 40.0,
+              color: Colors.white,
+            ),
+          ],
+        ),
       ),
     );
   }
