a9cc2a2c7533cae34ef9d7c04e16c415d656cc3f
Keertiraj Laxman Malik <KeertirajLaxmanMalik@fico.com>
2021-02-26 22:32:38 +0530

    renamed Foods to Foods and Drinks

--- lib/models/expense_data.dart
+++ lib/models/expense_data.dart
@@ -3,7 +3,7 @@ import 'package:pocket_friend/models/expense.dart';
 class ExpenseData {
   List<Expense> expenseAmount = [
     Expense(expense: 'Electricity', amount: '270'),
-    Expense(expense: 'Food', amount: '470'),
+    Expense(expense: 'Food & Drinks', amount: '470'),
     Expense(expense: 'Home', amount: '570'),
   ];
 
