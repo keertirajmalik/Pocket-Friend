478b893549ebfc6beb127c44d0ca022c473ef8b0
Keertiraj Laxman Malik <KeertirajLaxmanMalik@fico.com>
2021-03-01 22:55:31 +0530

    added the Currency button along with text field to enter the amount

--- lib/screens/add_transaction_screen.dart
+++ lib/screens/add_transaction_screen.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class AddTransactionScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final _todayDate = TextEditingController();
+    _todayDate.text = DateFormat('dd MMM y').format(DateTime.now());
     return Scaffold(
       backgroundColor: Color(0xff27c791),
       body: Column(
@@ -16,9 +19,8 @@ class AddTransactionScreen extends StatelessWidget {
               decoration: BoxDecoration(
                 color: Colors.white,
                 borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(30.0),
-                  topRight: Radius.circular(30.0),
-                ),
+                    topLeft: Radius.circular(30.0),
+                    topRight: Radius.circular(30.0)),
               ),
               child: Column(
                 children: [
@@ -34,10 +36,32 @@ class AddTransactionScreen extends StatelessWidget {
                       ),
                     ],
                   ),
-                  TextField(
-                    decoration: InputDecoration(
-                        border: InputBorder.none,
-                        hintText: 'Enter a search term'),
+                  SizedBox(height: 20.0),
+                  Row(
+                    children: [
+                      MaterialButton(
+                        onPressed: () {},
+                        color: Color(0xff27c791),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(18.0)),
+                        child: Text('INR',
+                            style:
+                                TextStyle(color: Colors.white, fontSize: 18.0)),
+                      ),
+                      SizedBox(width: 5.0),
+                      Expanded(
+                        child: TextField(
+                          decoration: InputDecoration(
+                            border: InputBorder.none,
+                          ),
+                          cursorColor: Color(0xff27c791),
+                          autofocus: true,
+                          keyboardType: TextInputType.number,
+                          style: TextStyle(
+                              fontSize: 25.0, fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                    ],
                   ),
                 ],
               ),
