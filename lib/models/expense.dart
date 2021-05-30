036e5e24b20d36d3cf89d67fc424ded1dab3e6c1
Keertiraj Malik <keertirajmalik@gmail.com>
2021-05-30 20:00:27 +0530

    removed the id from the Expense class

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -2,7 +2,6 @@ class Expense {
   final String? expenseType;
   final String? amount;
   final String? date;
-  final int id;
 
-  Expense({this.expenseType, this.amount, this.date, required this.id});
+  Expense({this.expenseType, this.amount, this.date});
 }
